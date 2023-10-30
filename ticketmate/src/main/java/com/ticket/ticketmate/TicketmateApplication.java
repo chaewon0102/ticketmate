36f7f96fc1957775d565d856f623d371f9bad7a4
hcw <hcw@LAPTOP-444CB8NU>
2023-10-30 16:21:43 +0900

    first completed

---
+++ ticketmate/src/main/java/com/ticket/ticketmate/TicketmateApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.ticketmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmateApplication.class, args);
+	}
+
+}
---
+++ ticketmate/src/test/java/com/ticket/ticketmate/TicketmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.ticketmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
